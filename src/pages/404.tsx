d6015aee5cc3dc3109fd2fe2a6524ab4fb64e499
bulian1311 <bulian1311@gmail.com>
2022-06-10 14:51:41 +0300

    Page 404.

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import { Block, Headline } from "../components/ui";
+
+const NotFound: NextPage = () => {
+  return (
+    <>
+      <Block>
+        <Headline tag="h2">Такай страницы нет!</Headline>
+        <Link href="/">
+          <a>Перейти на главную.</a>
+        </Link>
+      </Block>
+    </>
+  );
+};
+export default NotFound;
