4afd9341cf1292b8d9e1169824b9b2ee95ee11a2
bulian1311 <bulian1311@gmail.com>
2022-06-10 14:51:07 +0300

    App environments.

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const nextConfig = {
     ]
   },
   env: {
-    API_URL:process.env.API_URL,
-    API_VERSION:process.env.API_VERSION,
+    API_URL: process.env.API_URL,
+    API_VERSION: process.env.API_VERSION,
     API_KEY: process.env.API_KEY,
-    API_LANG:process.env.API_LANG
+    API_LANG: process.env.API_LANG
   }
 }
 
