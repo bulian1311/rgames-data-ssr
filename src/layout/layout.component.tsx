1d5323693ee3773544cf06e4f0899534870fe995
bulian1311 <bulian1311@gmail.com>
2022-09-12 14:26:13 +0300

    App layout.

--- src/layout/layout.component.tsx
+++ src/layout/layout.component.tsx
@@ -6,7 +6,7 @@ import { Footer } from './footer';
 
 export const Layout = ({ children }: Props): JSX.Element => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 w-full h-full z-[-1]">
         <Image
           priority
@@ -20,11 +20,11 @@ export const Layout = ({ children }: Props): JSX.Element => {
 
       <Navbar />
 
-      <main className="container pt-20 mx-auto md:pt-32 mb-20 text-slate-200">
+      <main className="container pt-20 mx-auto md:pt-32 mb-20 text-slate-200 grow">
         {children}
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 };
